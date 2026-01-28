d6e2e833dd2649c8daa1079b5ce18417222dabef
Tiago Monteiro <9205078+tiaguito@users.noreply.github.com>
2026-01-28 19:16:05 +0900

    uncommented code in app/main.go

--- app/main.go
+++ app/main.go
@@ -46,18 +46,16 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		// TODO: Uncomment the code below to pass the first stage
-		//
-		// bencodedValue := os.Args[2]
-		//
-		// decoded, err := decodeBencode(bencodedValue)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	return
-		// }
-		//
-		// jsonOutput, _ := json.Marshal(decoded)
-		// fmt.Println(string(jsonOutput))
+		bencodedValue := os.Args[2]
+
+		decoded, err := decodeBencode(bencodedValue)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		jsonOutput, _ := json.Marshal(decoded)
+		fmt.Println(string(jsonOutput))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
