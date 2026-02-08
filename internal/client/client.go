c07d12aaff6f778bec005a73fb57948e8260289b
Tiago Monteiro <9205078+tiaguito@users.noreply.github.com>
2026-02-08 10:45:10 +0900

    extended capabilities of client

--- internal/client/client.go
+++ internal/client/client.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/handshake"
+	"github.com/codecrafters-io/bittorrent-starter-go/internal/messages"
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/peers"
 )
 
@@ -13,6 +14,8 @@ type Bitfield []byte
 
 type Client struct {
 	Conn      net.Conn
+	Bitfield  Bitfield
+	Choked    bool
 	Peer      peers.Peer
 	InfoHash  [20]byte
 	PeerID    [20]byte
@@ -39,6 +42,20 @@ func doHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake
 	return res, nil
 }
 
+func readBitfield(conn net.Conn) (Bitfield, error) {
+	msg, err := messages.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg == nil {
+		return nil, err
+	}
+	if msg.ID != messages.MsgBitfield {
+		return nil, fmt.Errorf("expected bitfield but got ID %d", msg.ID)
+	}
+	return msg.Payload, nil
+}
+
 func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 	conn, err := net.Dial("tcp", peer.String())
 	if err != nil {
@@ -51,11 +68,42 @@ func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 		return nil, err
 	}
 
+	bf, err := readBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
 	return &Client{
 		Conn:      conn,
+		Bitfield:  bf,
+		Choked:    true,
 		Peer:      peer,
 		InfoHash:  infoHash,
 		PeerID:    peerID,
 		Handshake: handshakeResp,
 	}, nil
+}
+
+func (c *Client) Read() (*messages.Message, error) {
+	msg, err := messages.Read(c.Conn)
+	return msg, err
+}
+
+func (c *Client) SendUnchoke() error {
+	msg := messages.Message{ID: messages.MsgUnchoke}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendRequest(index, begin, length int) error {
+	req := messages.FormatRequest(index, begin, length)
+	_, err := c.Conn.Write(req.Serialize())
+	return err
+}
+
+func (c *Client) SendInterested() error {
+	msg := messages.Message{ID: messages.MsgInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
 }
--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/handshake"
+	"github.com/codecrafters-io/bittorrent-starter-go/internal/messages"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -69,4 +70,72 @@ func TestDoHandshake(t *testing.T) {
 			assert.Equal(t, h, test.output)
 		}
 	}
+}
+
+func TestRead(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+
+	msgBytes := []byte{
+		0x00, 0x00, 0x00, 0x05,
+		4,
+		0x00, 0x00, 0x05, 0x3c,
+	}
+	expected := &messages.Message{
+		ID:      messages.MsgHave,
+		Payload: []byte{0x00, 0x00, 0x05, 0x3c},
+	}
+	_, err := serverConn.Write(msgBytes)
+	require.Nil(t, err)
+
+	msg, err := client.Read()
+	assert.Equal(t, expected, msg)
+}
+
+func TestSendRequest(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendRequest(1, 2, 3)
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x0d,
+		6,
+		0x00, 0x00, 0x00, 0x01,
+		0x00, 0x00, 0x00, 0x02,
+		0x00, 0x00, 0x00, 0x03,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendInterested(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendInterested()
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x01,
+		2,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendUnchoke(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendUnchoke()
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x01,
+		1,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
 }
